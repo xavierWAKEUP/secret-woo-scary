b39d90bae2ceca7b721a5ab74b8de9e15226abfd
xavierWAKEUP <123764508+xavierWAKEUP@users.noreply.github.com>
2023-02-08 10:54:42 -0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,9 +1,19 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+  header("Location: index.php");
+  exit;
+}
+?>
 <html>
-  <head>
-    <title>Welcome Page</title>
-  </head>
-  <body>
-    <h1>Welcome!</h1>
-    <p>You have successfully logged in.</p>
-  </body>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Welcome Page</title>
+</head>
+<body>
+  <h1>Welcome to the protected area!</h1>
+  <p>You have successfully logged in.</p>
+  <a href="logout.php">Logout</a>
+</body>
 </html>
