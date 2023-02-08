4c90b86c0bb0e81583beece848d94add0778a884
xavierWAKEUP <123764508+xavierWAKEUP@users.noreply.github.com>
2023-02-08 10:52:52 -0800

    Update login.php

--- login.php
+++ login.php
@@ -1,29 +1,32 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $username = $_POST["username"];
-  $password = $_POST["password"];
+session_start();
 
-  if ($username === "Xavier" && $password === "Xavier232") {
-    header("Location: https://xavierwakeup.github.io/secret-woo-scary/welcome.php");
+if(isset($_POST['submit'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  if ($username == "admin" && $password == "password") {
+    $_SESSION['logged_in'] = true;
+    header("Location: welcome.php");
     exit;
   } else {
-    echo "Incorrect username or password. Please try again.";
+    $error = "Incorrect username or password. Please try again.";
   }
 }
 ?>
-
 <html>
-  <head>
-    <title>Login Page</title>
-  </head>
-  <body>
-    <h1>Login</h1>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-      <label for="username">Username:</label>
-      <input type="text" id="username" name="username"><br><br>
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password"><br><br>
-      <input type="submit" value="Submit">
-    </form>
-  </body>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Login Page</title>
+</head>
+<body>
+  <?php if (isset($error)): ?>
+    <p><?php echo $error; ?></p>
+  <?php endif; ?>
+  <form method="post">
+    <input type="text" name="username" placeholder="Username">
+    <input type="password" name="password" placeholder="Password">
+    <input type="submit" name="submit" value="Submit">
+  </form>
+</body>
 </html>
