cc6e06044201f7993aa6d598358094b37da930f3
xavierWAKEUP <123764508+xavierWAKEUP@users.noreply.github.com>
2023-02-08 14:22:23 -0800

    Update login.php

--- login.php
+++ login.php
@@ -1,32 +1,46 @@
 <?php
 session_start();
 
-if(isset($_POST['submit'])) {
+if (isset($_POST['username']) && isset($_POST['password'])) {
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  if ($username == "admin" && $password == "password") {
-    $_SESSION['logged_in'] = true;
+  // Connect to the database
+  $db = mysqli_connect('localhost', 'db_user', 'db_password', 'db_name');
+
+  // Check if the username and password are correct
+  $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+  $result = mysqli_query($db, $query);
+
+  if (mysqli_num_rows($result) == 1) {
+    // Set the session variable
+    $_SESSION['username'] = $username;
+
+    // Redirect to the welcome page
     header("Location: welcome.php");
-    exit;
   } else {
-    $error = "Incorrect username or password. Please try again.";
+    // Show an error message
+    echo "Invalid username or password";
   }
 }
 ?>
+
+<!DOCTYPE html>
 <html>
 <head>
   <link rel="stylesheet" type="text/css" href="style.css">
   <title>Login Page</title>
 </head>
 <body>
-  <?php if (isset($error)): ?>
-    <p><?php echo $error; ?></p>
-  <?php endif; ?>
-  <form method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="password" placeholder="Password">
-    <input type="submit" name="submit" value="Submit">
+  <h1>Login</h1>
+  <form action="login.php" method="post">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required>
+    <br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+    <br>
+    <input type="submit" value="Submit">
   </form>
 </body>
 </html>
